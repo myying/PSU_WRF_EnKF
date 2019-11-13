ed6ac4f2adc1bb7bf2a85a107c00d32d3e67243d
Yue Ying <hardyying@gmail.com>
2019-11-13 16:32:22 -0700

    let scale_decompose output current scale and smaller scale

--- EnKF/src/scale_decompose.f
+++ EnKF/src/scale_decompose.f
@@ -5,7 +5,9 @@ use mpi_module
 use mapinfo_define
 use netcdf
 use scale_utils
-integer            :: i_unit=80010
+!!!read intermediate updated state 900??, decompose into
+!!!current scale copmonent 500?? and smaller scale component 600??
+integer            :: i_unit=90010, s_unit=50010, s1_unit=60010
 integer            :: unit
 integer            :: ie,ix,jx,kx,len,i,j,k,n,ni,nj,nk
 integer            :: ii,jj,kk,nn,m,nv,nm,fid
@@ -13,7 +15,7 @@ real               :: member_per_cpu
 character (len=10) :: wrf_file
 real, allocatable, dimension(:,:) :: dat2d
 real, allocatable, dimension(:,:,:) :: dat3d
-real, allocatable, dimension(:,:,:,:,:) :: x,xout
+real, allocatable, dimension(:,:,:,:,:) :: x,xout,xout1
 type(proj_info)    :: proj
 character (len=80) :: times
 !----------------------------------------------------------------
@@ -29,13 +31,7 @@ call read_namelist(ix, jx, kx)
 !!only using nmcpu here, no domain decomposition (because fft2 requires whole domain)
 member_per_cpu=real(numbers_en+1)/nmcpu
 nm=ceiling(member_per_cpu)
-if ( my_proc_id == 0 ) then
-  write(*,*) '---------------------------------------------------'
-  print*, num_scales
-  print*, krange
-endif
 
-!-- allocate wrf variables, 2d->x2, 3d->x
 nv = 0
 do m=1,30
   if(len_trim(enkfvar(m))>=1) nv=nv+1
@@ -43,9 +39,15 @@ enddo
 ni=ix+1
 nj=jx+1
 nk=kx+1
-allocate(x(ni,nj,nk,nv,nm))
-allocate(xout(ni,nj,nk,nv,nm))
+allocate(x(ni,nj,nk,nv,nm),xout(ni,nj,nk,nv,nm),xout1(ni,nj,nk,nv,nm))
 x=0.
+
+!!!krange should have ns-1 entries, each is the wavenumber separating scales.
+if ( my_proc_id == 0 ) then
+  write(*,*) '---------------------------------------------------'
+  print*, num_scales
+  print*, krange(1:num_scales-1)
+endif
 
 !!! Read in initial Ensemble in x
 if ( my_proc_id == 0 ) write(*,*)'Read in initial Ensemble in x'
@@ -69,6 +71,7 @@ enddo
 
 !!! Get scale component
 xout=x
+xout1=x
 do n=1,nm
   ie=(n-1)*my_proc_id+1
   if(ie<=numbers_en) then
@@ -76,8 +79,8 @@ do n=1,nm
       call wrf_var_dimension(wrf_file,enkfvar(m),ix,jx,kx,ii,jj,kk)
       nn=min(ii,jj)
       do k=1,kk
-        call scale_bandpass(x(1:nn,1:nn,k,m,n),krange(1:num_scales+1), &
-                            current_scale,xout(1:nn,1:nn,k,m,n))
+        call scale_bandpass(x(1:nn,1:nn,k,m,n),krange(1:num_scales-1), &
+                            current_scale,xout(1:nn,1:nn,k,m,n),xout1(1:nn,1:nn,k,m,n))
       enddo
     enddo
   endif
@@ -87,7 +90,7 @@ enddo
 do n=1,nm
   ie=(n-1)*my_proc_id+1
   if(ie<=numbers_en) then
-    write(wrf_file,'(a5,i5.5)') 'fort.',70010+ie
+    write(wrf_file,'(a5,i5.5)') 'fort.',s_unit+ie
     call open_file(wrf_file,nf_write,fid)
     do m=1,nv
       call wrf_var_dimension(wrf_file,enkfvar(m),ix,jx,kx,ii,jj,kk)
@@ -104,10 +107,30 @@ do n=1,nm
     call close_file(fid)
   endif
 enddo
+do n=1,nm
+  ie=(n-1)*my_proc_id+1
+  if(ie<=numbers_en) then
+    write(wrf_file,'(a5,i5.5)') 'fort.',s1_unit+ie
+    call open_file(wrf_file,nf_write,fid)
+    do m=1,nv
+      call wrf_var_dimension(wrf_file,enkfvar(m),ix,jx,kx,ii,jj,kk)
+      allocate(dat3d(ii,jj,kk))
+      dat3d=0.
+      dat3d(1:ii,1:jj,1:kk)=xout1(1:ii,1:jj,1:kk,m,n)
+      if(kk>1) then
+        call write_variable3d(fid,enkfvar(m),ii,jj,kk,1,dat3d)
+      else if (kk==1) then
+        call write_variable2d(fid,enkfvar(m),ii,jj,1,dat3d)
+      endif
+      deallocate(dat3d)
+    enddo
+    call close_file(fid)
+  endif
+enddo
 
 if ( my_proc_id == 0 ) write(*,'(a)')' Successful completion of scale_decompose.exe'
 
-deallocate(x)
+deallocate(x,xout,xout1)
 call parallel_finish()
 
 end program scale_decompose
--- EnKF/src/scale_utils.f
+++ EnKF/src/scale_utils.f
@@ -7,99 +7,45 @@ function scale_response(k,krange,s) result(r)
   real,dimension(:,:) :: k
   integer,dimension(:) :: krange
   real,dimension(size(k,1),size(k,2)) :: r
-  ns=size(krange)
+  ns=size(krange)+1
   if(s<1 .or. s>ns) then
     print *,'band not within range!'
     stop
   end if
-
   r=0.0
-  if(s==ns) then
-    where(k>=krange(s)) r=1.0
+  if(s==1) then
+    where(k<krange(s)) r=1.0
+  else if(s==ns) then
+    where(k>=krange(s-1)) r=1.0
   else
-    where(k>=krange(s) .and. k<krange(s+1)) r=1.0
+    where(k>=krange(s-1) .and. k<krange(s)) r=1.0
   end if
 end function scale_response
 
 function scale_response1(k,krange,s) result(r)
-  integer :: s,center_k,left_k,right_k
+  integer :: s
   real,dimension(:,:) :: k
   integer,dimension(:) :: krange
   real,dimension(size(k,1),size(k,2)) :: r
-  real,parameter :: pi=3.14159265358979
-  ns=size(krange)
+  ns=size(krange)+1
   if(s<1 .or. s>ns) then
     print *,'band not within range!'
     stop
   end if
-
   r=0.0
-  center_k=krange(s)
-  if(s.eq.1) then
-    where(k<=center_k) r=1.0
+  if(s==ns) then
+    where(k>=krange(s-1)) r=1.0
   else
-    left_k=krange(s-1)
-    where(k<=center_k .and. k>=left_k) &
-      r=cos((k-center_k)*(0.5*pi/(left_k-center_k)))**2
-  end if
-  if(s.eq.ns) then
-    where(k>=center_k) r=1.0
-  else
-    right_k=krange(s+1)
-    where(k>=center_k .and. k<=right_k) &
-      r=cos((k-center_k)*(0.5*pi/(right_k-center_k)))**2
+    where(k>=krange(s)) r=1.0
   end if
 end function scale_response1
 
-!subroutine separate_scales(u,krange,ums)
-!  use,intrinsic :: iso_c_binding
-!  use fft_pack
-!  real,dimension(:,:,:) :: u
-!  integer,dimension(:) :: krange
-!  real,dimension(size(u,1),size(u,2),size(u,3),size(krange)) :: ums
-!  integer :: nx,ny,nz,ns, n,i,z,s
-!  integer,dimension(size(u,1)) :: kx1
-!  integer,dimension(size(u,2)) :: ky1
-!  real,dimension(size(u,1),size(u,2)) :: kx,ky
-!  real,dimension(size(u,1),size(u,2)) :: keff,flt
-!  complex(C_DOUBLE_COMPLEX),dimension(size(u,1),size(u,2)) :: uspec
-!  nx=size(u,1)
-!  ny=size(u,2)
-!  nz=size(u,3)
-!  ns=size(krange)
-!  if(ns.eq.1) then
-!    ums(:,:,:,1)=u
-!  else
-!    if(mod(nx,2).eq.0) then
-!      kx1=(/(i,i=0,ceiling(real(nx-1)/2)),(i,i=-ceiling(real(nx-1)/2)+1,-1)/)
-!    else
-!      kx1=(/(i,i=0,ceiling(real(nx-1)/2)),(i,i=-ceiling(real(nx-1)/2),-1)/)
-!    end if
-!    if(mod(ny,2).eq.0) then
-!      ky1=(/(i,i=0,ceiling(real(ny-1)/2)),(i,i=-ceiling(real(ny-1)/2)+1,-1)/)
-!    else
-!      ky1=(/(i,i=0,ceiling(real(ny-1)/2)),(i,i=-ceiling(real(ny-1)/2),-1)/)
-!    end if
-!    call grid2d(real(kx1),real(ky1),kx,ky)
-!    n=max(nx,ny)
-!    keff=sqrt((kx*(n/nx))**2+(ky*(n/ny))**2)
-!    do s=1,ns
-!      flt=scale_response1(keff,krange,s)
-!      do z=1,nz
-!        uspec=fft2(dcmplx(u(:,:,z),0.0))/(nx*ny)
-!        uspec=uspec*flt
-!        ums(:,:,z,s)=real(ifft2(uspec))
-!      end do
-!    end do
-!  end if
-!end subroutine separate_scales
-
-subroutine scale_bandpass(u,krange,s,us)
+subroutine scale_bandpass(u,krange,s,us,us1)
   use,intrinsic :: iso_c_binding
   use fft_pack
   real,dimension(:,:) :: u
   integer,dimension(:) :: krange
-  real,dimension(size(u,1),size(u,2)),intent(out) :: us
+  real,dimension(size(u,1),size(u,2)),intent(out) :: us,us1
   integer :: nx,ny,ns, n,i,z,s
   integer,dimension(size(u,1)) :: kx1
   integer,dimension(size(u,2)) :: ky1
@@ -126,10 +72,11 @@ subroutine scale_bandpass(u,krange,s,us)
     call grid2d(real(kx1),real(ky1),kx,ky)
     n=max(nx,ny)
     keff=sqrt((kx*(n/nx))**2+(ky*(n/ny))**2)
-    flt=scale_response(keff,krange,s)
     uspec=fft2(dcmplx(u,0.0))/(nx*ny)
-    uspec=uspec*flt
-    us=real(ifft2(uspec))
+    flt=scale_response(keff,krange,s)
+    us=real(ifft2(uspec*flt))
+    flt=scale_response1(keff,krange,s)
+    us1=real(ifft2(uspec*flt))
   end if
 end subroutine scale_bandpass
 
@@ -142,17 +89,5 @@ subroutine grid2d(xind,yind,x,y)
   y=spread(yind,1,nx)
   x=spread(xind,2,ny)
 end subroutine grid2d
-
-subroutine grid3d(xind,yind,zind,x,y,z)
-  real,dimension(:),intent(in) :: xind,yind,zind
-  real,dimension(size(xind),size(yind),size(zind)),intent(out) :: x,y,z
-  integer :: i,j,k,nx,ny,nz
-  nx=size(xind)
-  ny=size(yind)
-  nz=size(zind)
-  x=spread(spread(xind,2,ny),3,nz)
-  y=spread(spread(yind,1,nx),3,nz)
-  z=spread(spread(zind,1,nx),2,ny)
-end subroutine grid3d
 
 end module scale_utils
