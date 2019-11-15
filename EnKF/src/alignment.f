69db50446b634bf8f354fbf3ea99104baa24384c
Yue Ying <hardyying@gmail.com>
2019-11-15 16:56:11 -0700

    simplify scale_decompose

--- EnKF/src/alignment.f
+++ EnKF/src/alignment.f
@@ -32,17 +32,13 @@ call read_namelist(ix, jx, kx)
 !!only using nmcpu here, no domain decomposition (because requires whole domain)
 member_per_cpu=real(numbers_en+1)/nmcpu
 nm=ceiling(member_per_cpu)
-!if ( my_proc_id == 0 ) then
-!  write(*,*) '---------------------------------------------------'
-!  print*, num_scales
-!  print*, krange
-!endif
 
 !-- allocate wrf variables
 nv = 0
 do m=1,30
   if(len_trim(enkfvar(m))>=1) nv=nv+1
 enddo
+if ( my_proc_id == 0 ) print*, 'nm=',nm, 'nv=',nv
 
 !!!!!BETTER PARALLEL:
 !!!! loop over members and variables????
@@ -50,6 +46,8 @@ do n=1,nm
   ie=(n-1)*my_proc_id+1
   if(ie<=numbers_en) then
     do m=1,nv
+      if(my_proc_id==0) print*, 'processing ', enkfvar(m)
+
       write(wrf_file,'(a5,i5.5)') 'fort.',b_unit+ie
       call wrf_var_dimension(wrf_file,enkfvar(m),ix,jx,kx,ii,jj,kk)
       allocate(u(ii,jj),v(ii,jj))
@@ -81,7 +79,8 @@ do n=1,nm
 
       do k=1,kk
         !!! Compute displacement vectors using Horn Schunck
-        call optical_flow_HS(xb(:,:,k),xa(:,:,k),100.,u,v)
+        !call optical_flow_HS(xb(:,:,k),xa(:,:,k),20.,u,v)
+        u=0.; v=0.
         !!! Warp smaller-scale field xs -> xsw
         call warp_state(xs(:,:,k),xsw(:,:,k),u,v)
       enddo
@@ -95,8 +94,10 @@ do n=1,nm
       endif
 
       !!!!add increment and write back
-      xout=xout+xsw-xs!+xa-xb
+      !xout=xout+xsw-xs+xa-xb
+      xout=xout-xs-xb
 
+      write(wrf_file,'(a5,i5.5)') 'fort.',o_unit+ie
       call open_file(wrf_file,nf_write,fid)
       if(kk>1) then
         call write_variable3d(fid,enkfvar(m),ii,jj,kk,1,xout)
--- EnKF/src/scale_decompose.f
+++ EnKF/src/scale_decompose.f
@@ -9,13 +9,11 @@ use multiscale_utils
 !!!current scale copmonent 500?? and smaller scale component 600??
 integer            :: i_unit=90010, s_unit=50010, s1_unit=60010
 integer            :: unit
-integer            :: ie,ix,jx,kx,len,i,j,k,n,ni,nj,nk
+integer            :: ie,ix,jx,kx,len,i,j,k,n
 integer            :: ii,jj,kk,nn,m,nv,nm,fid
 real               :: member_per_cpu
 character (len=10) :: wrf_file
-real, allocatable, dimension(:,:) :: dat2d
-real, allocatable, dimension(:,:,:) :: dat3d
-real, allocatable, dimension(:,:,:,:,:) :: x,xout,xout1
+real, allocatable, dimension(:,:,:) :: x,xout,xout1
 type(proj_info)    :: proj
 character (len=80) :: times
 !----------------------------------------------------------------
@@ -36,11 +34,6 @@ nv = 0
 do m=1,30
   if(len_trim(enkfvar(m))>=1) nv=nv+1
 enddo
-ni=ix+1
-nj=jx+1
-nk=kx+1
-allocate(x(ni,nj,nk,nv,nm),xout(ni,nj,nk,nv,nm),xout1(ni,nj,nk,nv,nm))
-x=0.
 
 !!!krange should have ns-1 entries, each is the wavenumber separating scales.
 if ( my_proc_id == 0 ) then
@@ -49,88 +42,57 @@ if ( my_proc_id == 0 ) then
   print*, krange(1:num_scales-1)
 endif
 
-!!! Read in initial Ensemble in x
-if ( my_proc_id == 0 ) write(*,*)'Read in initial Ensemble in x'
 do n=1,nm
   ie=(n-1)*my_proc_id+1
   if(ie<=numbers_en) then
-    write(wrf_file,'(a5,i5.5)') 'fort.',i_unit+ie
     do m=1,nv
-      call wrf_var_dimension(wrf_file,enkfvar(m),ix,jx,kx,ii,jj,kk)
-      allocate(dat3d(ii,jj,kk))
-      if(kk>1) then
-        call get_variable3d(wrf_file,enkfvar(m),ii,jj,kk,1,dat3d)
-      else if (kk==1) then
-        call get_variable2d(wrf_file,enkfvar(m),ii,jj,1,dat3d)
-      endif
-      x(1:ii,1:jj,1:kk,m,n)=dat3d(1:ii,1:jj,1:kk)
-      deallocate(dat3d)
-    enddo
-  endif
-enddo
+      if(my_proc_id==0) print*, 'processing ', enkfvar(m)
 
-!!! Get scale component
-xout=x
-xout1=x
-do n=1,nm
-  ie=(n-1)*my_proc_id+1
-  if(ie<=numbers_en) then
-    do m=1,nv
+      write(wrf_file,'(a5,i5.5)') 'fort.',i_unit+ie
       call wrf_var_dimension(wrf_file,enkfvar(m),ix,jx,kx,ii,jj,kk)
+      allocate(x(ii,jj,kk),xout(ii,jj,kk),xout1(ii,jj,kk))
+      x=0.
+
+      !!! Read in initial Ensemble in x
+      if(kk>1) then
+        call get_variable3d(wrf_file,enkfvar(m),ii,jj,kk,1,x)
+      else if (kk==1) then
+        call get_variable2d(wrf_file,enkfvar(m),ii,jj,1,x)
+      endif
+
+      !!! Get scale component
+      xout=x
+      xout1=x
       nn=min(ii,jj)
       do k=1,kk
-        call scale_bandpass(x(1:nn,1:nn,k,m,n),krange(1:num_scales-1), &
-                            current_scale,xout(1:nn,1:nn,k,m,n),xout1(1:nn,1:nn,k,m,n))
+        call scale_bandpass(x(1:nn,1:nn,k),krange(1:num_scales-1), &
+                            current_scale,xout(1:nn,1:nn,k),xout1(1:nn,1:nn,k))
       enddo
-    enddo
-  endif
-enddo
 
-!!! output netcdf file
-do n=1,nm
-  ie=(n-1)*my_proc_id+1
-  if(ie<=numbers_en) then
-    write(wrf_file,'(a5,i5.5)') 'fort.',s_unit+ie
-    call open_file(wrf_file,nf_write,fid)
-    do m=1,nv
-      call wrf_var_dimension(wrf_file,enkfvar(m),ix,jx,kx,ii,jj,kk)
-      allocate(dat3d(ii,jj,kk))
-      dat3d=0.
-      dat3d(1:ii,1:jj,1:kk)=xout(1:ii,1:jj,1:kk,m,n)
+      !!!output netcdf file
+      write(wrf_file,'(a5,i5.5)') 'fort.',s_unit+ie
+      call open_file(wrf_file,nf_write,fid)
       if(kk>1) then
-        call write_variable3d(fid,enkfvar(m),ii,jj,kk,1,dat3d)
+        call write_variable3d(fid,enkfvar(m),ii,jj,kk,1,xout)
       else if (kk==1) then
-        call write_variable2d(fid,enkfvar(m),ii,jj,1,dat3d)
+        call write_variable2d(fid,enkfvar(m),ii,jj,1,xout)
       endif
-      deallocate(dat3d)
-    enddo
-    call close_file(fid)
-  endif
-enddo
-do n=1,nm
-  ie=(n-1)*my_proc_id+1
-  if(ie<=numbers_en) then
-    write(wrf_file,'(a5,i5.5)') 'fort.',s1_unit+ie
-    call open_file(wrf_file,nf_write,fid)
-    do m=1,nv
-      call wrf_var_dimension(wrf_file,enkfvar(m),ix,jx,kx,ii,jj,kk)
-      allocate(dat3d(ii,jj,kk))
-      dat3d=0.
-      dat3d(1:ii,1:jj,1:kk)=xout1(1:ii,1:jj,1:kk,m,n)
+      call close_file(fid)
+      write(wrf_file,'(a5,i5.5)') 'fort.',s1_unit+ie
+      call open_file(wrf_file,nf_write,fid)
       if(kk>1) then
-        call write_variable3d(fid,enkfvar(m),ii,jj,kk,1,dat3d)
+        call write_variable3d(fid,enkfvar(m),ii,jj,kk,1,xout1)
       else if (kk==1) then
-        call write_variable2d(fid,enkfvar(m),ii,jj,1,dat3d)
+        call write_variable2d(fid,enkfvar(m),ii,jj,1,xout1)
       endif
-      deallocate(dat3d)
+      call close_file(fid)
+      deallocate(x,xout,xout1)
     enddo
-    call close_file(fid)
   endif
 enddo
 
 if ( my_proc_id == 0 ) write(*,'(a)')' Successful completion of scale_decompose.exe'
 
-deallocate(x,xout,xout1)
 call parallel_finish()
 
 end program scale_decompose
