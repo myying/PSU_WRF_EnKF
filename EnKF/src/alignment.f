70d7f812743164b72c31fd7822fcf8702db292e6
Yue Ying <hardyying@gmail.com>
2019-11-25 14:29:20 -0700

    bug fix

--- EnKF/src/alignment.f
+++ EnKF/src/alignment.f
@@ -12,7 +12,7 @@ use multiscale_utils
 integer            :: b_unit=50010,a_unit=70010,s_unit=60010
 integer            :: i_unit=80010,o_unit=90010
 integer            :: ie,ix,jx,kx,len,i,j,k,n
-integer            :: ii,jj,kk,nn,m,nv,nm,fid
+integer            :: ii,jj,kk,nn,m,nv,fid
 character (len=10) :: wrf_file
 real, allocatable, dimension(:,:) :: u,v
 real, allocatable, dimension(:,:,:) :: xb,xa,xs,xsw,xin,xout,delx
@@ -30,7 +30,6 @@ call read_namelist(ix, jx, kx)
 
 !!only using nmcpu=nens here, no domain decomposition (because requires whole domain)
 !!ignore namelist parallel options
-nm=1
 if(nprocs .ne. numbers_en) then
   if(my_proc_id==0) print*,'ERROR: nprocs/=numbers_en'
 endif
@@ -45,80 +44,77 @@ if ( my_proc_id == 0 ) then
   print*, 'num_scales=',num_scales
   print*, 'current_scale=',current_scale
   print*, 'krange=',krange(1:num_scales-1)
-  print*, 'nm=',nm, 'nv=',nv
 endif
 
-do n=1,nm
-  ie=(n-1)*my_proc_id+1
-  if(ie<=numbers_en) then
-    do m=1,nv
-      if(my_proc_id==0) print*, 'processing ', enkfvar(m)
+ie=my_proc_id+1
+if(ie<=numbers_en) then
+  do m=1,nv
+    if(my_proc_id==0) print*, 'processing ', enkfvar(m)
 
-      write(wrf_file,'(a5,i5.5)') 'fort.',b_unit+ie
-      call wrf_var_dimension(wrf_file,enkfvar(m),ix,jx,kx,ii,jj,kk)
-      allocate(u(ii,jj),v(ii,jj))
-      allocate(xb(ii,jj,kk),xa(ii,jj,kk),xs(ii,jj,kk),xsw(ii,jj,kk))
-      allocate(xin(ii,jj,kk),xout(ii,jj,kk),delx(ii,jj,kk))
-      xb=0.; xa=0.; xs=0.; xsw=0.; xin=0.; xout=0.; delx=0.
+    write(wrf_file,'(a5,i5.5)') 'fort.',b_unit+ie
+    call wrf_var_dimension(wrf_file,enkfvar(m),ix,jx,kx,ii,jj,kk)
+    allocate(u(ii,jj),v(ii,jj))
+    allocate(xb(ii,jj,kk),xa(ii,jj,kk),xs(ii,jj,kk),xsw(ii,jj,kk))
+    allocate(xin(ii,jj,kk),xout(ii,jj,kk),delx(ii,jj,kk))
+    xb=0.; xa=0.; xs=0.; xsw=0.; xin=0.; xout=0.; delx=0.
 
-      !!! Read in prior ensemble at current scale
+    !!! Read in prior ensemble at current scale
+    if(kk>1) then
+      call get_variable3d(wrf_file,enkfvar(m),ii,jj,kk,1,xb)
+    else if (kk==1) then
+      call get_variable2d(wrf_file,enkfvar(m),ii,jj,1,xb)
+    endif
+
+    !!! Read in posterior ensemble at current scale
+    write(wrf_file,'(a5,i5.5)') 'fort.',a_unit+ie
+    if(kk>1) then
+      call get_variable3d(wrf_file,enkfvar(m),ii,jj,kk,1,xa)
+    else if (kk==1) then
+      call get_variable2d(wrf_file,enkfvar(m),ii,jj,1,xa)
+    endif
+
+    if(current_scale<num_scales) then
+      !!!read smaller scale component xs
+      write(wrf_file,'(a5,i5.5)') 'fort.',s_unit+ie
       if(kk>1) then
-        call get_variable3d(wrf_file,enkfvar(m),ii,jj,kk,1,xb)
+        call get_variable3d(wrf_file,enkfvar(m),ii,jj,kk,1,xs)
       else if (kk==1) then
-        call get_variable2d(wrf_file,enkfvar(m),ii,jj,1,xb)
+        call get_variable2d(wrf_file,enkfvar(m),ii,jj,1,xs)
       endif
 
-      !!! Read in posterior ensemble at current scale
-      write(wrf_file,'(a5,i5.5)') 'fort.',a_unit+ie
-      if(kk>1) then
-        call get_variable3d(wrf_file,enkfvar(m),ii,jj,kk,1,xa)
-      else if (kk==1) then
-        call get_variable2d(wrf_file,enkfvar(m),ii,jj,1,xa)
-      endif
+      do k=1,kk
+        !!! Compute displacement vectors using Horn Schunck
+        call optical_flow_HS(xb(:,:,k),xa(:,:,k),100.,u,v)
+        !!! Warp smaller-scale field xs -> xsw
+        call warp_state(xs(:,:,k),xsw(:,:,k),u,v)
+      enddo
+    end if
 
-      if(current_scale<num_scales) then
-        !!!read smaller scale component xs
-        write(wrf_file,'(a5,i5.5)') 'fort.',s_unit+ie
-        if(kk>1) then
-          call get_variable3d(wrf_file,enkfvar(m),ii,jj,kk,1,xs)
-        else if (kk==1) then
-          call get_variable2d(wrf_file,enkfvar(m),ii,jj,1,xs)
-        endif
+    !!!!read current intermediate state
+    write(wrf_file,'(a5,i5.5)') 'fort.',i_unit+ie
+    if(kk>1) then
+      call get_variable3d(wrf_file,enkfvar(m),ii,jj,kk,1,xin)
+    else if (kk==1) then
+      call get_variable2d(wrf_file,enkfvar(m),ii,jj,1,xin)
+    endif
 
-        do k=1,kk
-          !!! Compute displacement vectors using Horn Schunck
-          call optical_flow_HS(xb(:,:,k),xa(:,:,k),100.,u,v)
-          !!! Warp smaller-scale field xs -> xsw
-          call warp_state(xs(:,:,k),xsw(:,:,k),u,v)
-        enddo
-      end if
+    !!!!add increment and write back
+    delx=xa-xb
+    if(current_scale<num_scales) delx=delx+xsw-xs
+    xout=xin+delx
 
-      !!!!read current intermediate state
-      write(wrf_file,'(a5,i5.5)') 'fort.',i_unit+ie
-      if(kk>1) then
-        call get_variable3d(wrf_file,enkfvar(m),ii,jj,kk,1,xin)
-      else if (kk==1) then
-        call get_variable2d(wrf_file,enkfvar(m),ii,jj,1,xin)
-      endif
+    write(wrf_file,'(a5,i5.5)') 'fort.',o_unit+ie
+    call open_file(wrf_file,nf_write,fid)
+    if(kk>1) then
+      call write_variable3d(fid,enkfvar(m),ii,jj,kk,1,xout)
+    else if (kk==1) then
+      call write_variable2d(fid,enkfvar(m),ii,jj,1,xout)
+    endif
+    call close_file(fid)
 
-      !!!!add increment and write back
-      delx=xa-xb
-      if(current_scale<num_scales) delx=delx+xsw-xs
-      xout=xin+delx
-
-      write(wrf_file,'(a5,i5.5)') 'fort.',o_unit+ie
-      call open_file(wrf_file,nf_write,fid)
-      if(kk>1) then
-        call write_variable3d(fid,enkfvar(m),ii,jj,kk,1,xout)
-      else if (kk==1) then
-        call write_variable2d(fid,enkfvar(m),ii,jj,1,xout)
-      endif
-      call close_file(fid)
-
-      deallocate(u,v,xa,xb,xs,xsw,xin,xout,delx)
-    enddo
-  endif
-enddo
+    deallocate(u,v,xa,xb,xs,xsw,xin,xout,delx)
+  enddo
+endif
 if ( my_proc_id == 0 ) write(*,'(a)')' Successful completion of alignment.exe'
 call parallel_finish()
 
--- EnKF/src/scale_decompose.f
+++ EnKF/src/scale_decompose.f
@@ -10,7 +10,7 @@ use multiscale_utils
 integer            :: i_unit=90010, s_unit=50010, s1_unit=60010
 integer            :: unit
 integer            :: ie,ix,jx,kx,len,i,j,k,n
-integer            :: ii,jj,kk,nn,m,nv,nm,fid
+integer            :: ii,jj,kk,nn,m,nv,fid
 character (len=10) :: wrf_file
 real, allocatable, dimension(:,:,:) :: x,xout,xout1
 type(proj_info)    :: proj
@@ -27,7 +27,6 @@ call read_namelist(ix, jx, kx)
 
 !!only using nmcpu=nens here, no domain decomposition (because fft2 requires whole domain)
 !!ignore namelist parallel options
-nm=1
 if(nprocs .ne. numbers_en) then
   if(my_proc_id==0) print*,'ERROR: nprocs/=numbers_en'
 endif
@@ -43,57 +42,54 @@ if ( my_proc_id == 0 ) then
   print*, 'num_scales=',num_scales
   print*, 'current_scale=',current_scale
   print*, 'krange=',krange(1:num_scales-1)
-  print*, 'nm=',nm, 'nv=',nv
 endif
 
-do n=1,nm
-  ie=(n-1)*my_proc_id+1
-  if(ie<=numbers_en) then
-    do m=1,nv
-      if(my_proc_id==0) print*, 'processing ', enkfvar(m)
+ie=my_proc_id+1
+if(ie<=numbers_en) then
+  do m=1,nv
+    if(my_proc_id==0) print*, 'processing ', enkfvar(m)
 
-      write(wrf_file,'(a5,i5.5)') 'fort.',i_unit+ie
-      call wrf_var_dimension(wrf_file,enkfvar(m),ix,jx,kx,ii,jj,kk)
-      allocate(x(ii,jj,kk),xout(ii,jj,kk),xout1(ii,jj,kk))
-      x=0.
+    write(wrf_file,'(a5,i5.5)') 'fort.',i_unit+ie
+    call wrf_var_dimension(wrf_file,enkfvar(m),ix,jx,kx,ii,jj,kk)
+    allocate(x(ii,jj,kk),xout(ii,jj,kk),xout1(ii,jj,kk))
+    x=0.
 
-      !!! Read in initial Ensemble in x
-      if(kk>1) then
-        call get_variable3d(wrf_file,enkfvar(m),ii,jj,kk,1,x)
-      else if (kk==1) then
-        call get_variable2d(wrf_file,enkfvar(m),ii,jj,1,x)
-      endif
+    !!! Read in initial Ensemble in x
+    if(kk>1) then
+      call get_variable3d(wrf_file,enkfvar(m),ii,jj,kk,1,x)
+    else if (kk==1) then
+      call get_variable2d(wrf_file,enkfvar(m),ii,jj,1,x)
+    endif
 
-      !!! Get scale component
-      xout=x
-      xout1=x
-      nn=min(ii,jj)
-      do k=1,kk
-        call scale_bandpass(x(1:nn,1:nn,k),krange(1:num_scales-1), &
-                            current_scale,xout(1:nn,1:nn,k),xout1(1:nn,1:nn,k))
-      enddo
-
-      !!!output netcdf file
-      write(wrf_file,'(a5,i5.5)') 'fort.',s_unit+ie
-      call open_file(wrf_file,nf_write,fid)
-      if(kk>1) then
-        call write_variable3d(fid,enkfvar(m),ii,jj,kk,1,xout)
-      else if (kk==1) then
-        call write_variable2d(fid,enkfvar(m),ii,jj,1,xout)
-      endif
-      call close_file(fid)
-      write(wrf_file,'(a5,i5.5)') 'fort.',s1_unit+ie
-      call open_file(wrf_file,nf_write,fid)
-      if(kk>1) then
-        call write_variable3d(fid,enkfvar(m),ii,jj,kk,1,xout1)
-      else if (kk==1) then
-        call write_variable2d(fid,enkfvar(m),ii,jj,1,xout1)
-      endif
-      call close_file(fid)
-      deallocate(x,xout,xout1)
+    !!! Get scale component
+    xout=x
+    xout1=x
+    nn=min(ii,jj)
+    do k=1,kk
+      call scale_bandpass(x(1:nn,1:nn,k),krange(1:num_scales-1), &
+                          current_scale,xout(1:nn,1:nn,k),xout1(1:nn,1:nn,k))
     enddo
-  endif
-enddo
+
+    !!!output netcdf file
+    write(wrf_file,'(a5,i5.5)') 'fort.',s_unit+ie
+    call open_file(wrf_file,nf_write,fid)
+    if(kk>1) then
+      call write_variable3d(fid,enkfvar(m),ii,jj,kk,1,xout)
+    else if (kk==1) then
+      call write_variable2d(fid,enkfvar(m),ii,jj,1,xout)
+    endif
+    call close_file(fid)
+    write(wrf_file,'(a5,i5.5)') 'fort.',s1_unit+ie
+    call open_file(wrf_file,nf_write,fid)
+    if(kk>1) then
+      call write_variable3d(fid,enkfvar(m),ii,jj,kk,1,xout1)
+    else if (kk==1) then
+      call write_variable2d(fid,enkfvar(m),ii,jj,1,xout1)
+    endif
+    call close_file(fid)
+    deallocate(x,xout,xout1)
+  enddo
+endif
 
 if ( my_proc_id == 0 ) write(*,'(a)')' Successful completion of scale_decompose.exe'
 
