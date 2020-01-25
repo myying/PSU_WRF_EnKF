53ec2e55ce105c6d45229087af95141cf2ce2512
myying <yingyue@login1.stampede2.tacc.utexas.edu>
2020-01-25 14:57:22 -0600

    add determ forecast script

--- module_wrf.sh
+++ module_wrf.sh
@@ -16,6 +16,8 @@ echo running > stat
 
 export start_date=$DATE
 export run_minutes=$run_minutes_cycle
+export minute_off=`echo "(${DATE:8:2}*60+${DATE:10:2})%$LBC_INTERVAL" |bc`
+export LBDATE=`advance_time $DATE -$minute_off`
 
 echo "  Running WRFforecast..."
 
@@ -24,7 +26,7 @@ for r in 1; do
     touch rsl.error.0000
     if [[ `tail -n5 rsl.error.0000 |grep SUCCESS` ]]; then continue; fi
 
-    if [ $DATE == $LBDATE ]; then
+    if [[ $DATE == $LBDATE ]]; then
       export sst_update=1
     else
       export sst_update=0
@@ -33,24 +35,27 @@ for r in 1; do
     ####Running model
     ln -fs $WRF_DIR/run/* .
     rm -f namelist.*
-  
+
     for n in `seq 1 $MAX_DOM`; do
       dm=d`expr $n + 100 |cut -c2-`
-      if $RUN_ENKF; then
-        ln -fs ../../../fc/$DATE/wrfinput_${dm}_mean wrfinput_$dm
-      else
-        ln -fs ../../../fc/$PREVDATE/wrfinput_${dm}_`wrf_time_string $DATE`_mean wrfinput_$dm
-      fi
+      #if $RUN_ENKF; then
+      #  ln -fs ../../../fc/$DATE/wrfinput_${dm}_mean wrfinput_$dm
+      #else
+      #  ln -fs ../../../fc/$PREVDATE/wrfinput_${dm}_`wrf_time_string $DATE`_mean wrfinput_$dm
+      #fi
+      ncea ../../../fc/$DATE/wrfinput_${dm}_??? wrfinput_$dm
     done
 		ln -fs ../../../fc/wrfbdy_d01 wrfbdy_d01
 
     if [[ $SST_UPDATE == 1 ]]; then
-      ln -fs ../../../rc/$LBDATE/wrflowinp_d?? .
+      ln -fs ../../../rc/$DATE_START/wrflowinp_d?? .
     fi
 
     if $FOLLOW_STORM; then
-      cp $WORK_DIR/rc/$DATE/ij_parent_start .
-      cp $WORK_DIR/rc/$DATE/domain_moves .
+      $SCRIPT_DIR/calc_ij_parent_start.sh $DATE ij_parent_start >& follow_storm.log
+      watch_file ij_parent_start 1 $rundir
+      $SCRIPT_DIR/calc_domain_moves.sh $DATE `advance_time $DATE_END -$TCV_INTERVAL` domain_moves >& follow_storm.log
+      watch_file domain_moves 1 $rundir
     fi
     $SCRIPT_DIR/namelist_wrf.sh wrf > namelist.input
     $SCRIPT_DIR/job_submit.sh $wrf_ntasks 0 $HOSTPPN ./wrf.exe >& wrf.log
@@ -58,22 +63,22 @@ done
 
 #Check outputs
   watch_log rsl.error.0000 SUCCESS 1 $rundir
-  if $RUN_4DVAR; then
-    e_offset="$OBS_WIN_MIN 0"
-  else
-    e_offset="0"
-  fi
-  for i in $e_offset; do
-    outdate=`advance_time $NEXTDATE $i`
-    outfile=wrfinput_d01_`wrf_time_string $outdate`
-    mv $outfile $WORK_DIR/fc/$DATE/wrfinput_d01_`wrf_time_string $outdate`_mean
-    if [ $MAX_DOM -gt 1 ]; then
-      for n in `seq 1 $MAX_DOM`; do
-        dm=d`expr $n + 100 |cut -c2-`
-        outfile=wrfout_${dm}_`wrf_time_string $outdate`
-        mv $outfile $WORK_DIR/fc/$DATE/wrfinput_${dm}_`wrf_time_string $outdate`_mean
-      done
-    fi
-  done
+  #if $RUN_4DVAR; then
+  #  e_offset="$OBS_WIN_MIN 0"
+  #else
+  #  e_offset="0"
+  #fi
+  #for i in $e_offset; do
+  #  outdate=`advance_time $NEXTDATE $i`
+  #  outfile=wrfinput_d01_`wrf_time_string $outdate`
+  #  mv $outfile $WORK_DIR/fc/$DATE/wrfinput_d01_`wrf_time_string $outdate`_mean
+  #  if [ $MAX_DOM -gt 1 ]; then
+  #    for n in `seq 1 $MAX_DOM`; do
+  #      dm=d`expr $n + 100 |cut -c2-`
+  #      outfile=wrfout_${dm}_`wrf_time_string $outdate`
+  #      mv $outfile $WORK_DIR/fc/$DATE/wrfinput_${dm}_`wrf_time_string $outdate`_mean
+  #    done
+  #  fi
+  #done
 
 echo complete > stat
--- run_forecast.sh
+++ run_forecast.sh
@@ -3,113 +3,16 @@ source ~/.bashrc
 
 #load configuration files, functions, parameters
 cd $WORK/PSU_WRF_EnKF
-export CONFIG_FILE=$WORK/PSU_WRF_EnKF/config/$1
+export CONFIG_FILE=$WORK/PSU_WRF_EnKF/config/Patricia/control
 . $CONFIG_FILE
 . util.sh
 
 if [[ ! -d $WORK_DIR ]]; then mkdir -p $WORK_DIR; fi
 cd $WORK_DIR
 
-if [ $JOB_SUBMIT_MODE == 1 ]; then
-  if [[ $HOSTTYPE == "stampede" ]]; then
-    export total_ntasks=$SLURM_NTASKS
-  fi
-  if [[ $HOSTTYPE == "jet" ]]; then
-    export total_ntasks=$PBS_NP
-  fi
-else
-  export total_ntasks=9999999
-fi
+export total_ntasks=9999999
 
-#start cycling
 date
-export DATE=$DATE_START
-export PREVDATE=$DATE
-export NEXTDATE=$DATE
-
-while [[ $NEXTDATE -le $DATE_CYCLE_END ]]; do  #CYCLE LOOP
-
-  #TIME CALCULATION
-  if [[ $DATE == $DATE_START ]]; then
-    export run_minutes_cycle=`diff_time $DATE $DATE_CYCLE_START`
-  else
-    export run_minutes_cycle=$CYCLE_PERIOD
-  fi
-  export NEXTDATE=`advance_time $DATE $run_minutes_cycle`
-  if $FORECAST_TO_END; then
-    export run_minutes_forecast=`diff_time $DATE $DATE_END`
-  else
-    export run_minutes_forecast=`max $run_minutes_cycle $FORECAST_MINUTES`
-  fi
-  #LBDATE = Closest to DATE when FG is available
-  export minute_off=`echo "(${DATE:8:2}*60+${DATE:10:2})%$LBC_INTERVAL" |bc`
-  export LBDATE=`advance_time $DATE -$minute_off` 
-
-  echo "----------------------------------------------------------------------"
-  echo "CURRENT CYCLE: `wrf_time_string $DATE` => `wrf_time_string $NEXTDATE`"
-  mkdir -p {run,rc,fc,output,obs}/$DATE
-
-  #CLEAR ERROR TAGS
-  for d in `ls run/$DATE/`; do
-    if [[ `cat run/$DATE/$d/stat` == "error" ]]; then
-      echo waiting > run/$DATE/$d/stat
-    fi
-  done
-
-  #RUN COMPONENTS---------------------------------------
-
-  # ICBC
-  $SCRIPT_DIR/module_icbc.sh &
-#  # Ensemble initialization and forecast
-#  if $RUN_ENKF; then
-#    if [ $DATE == $DATE_START ]; then
-#      $SCRIPT_DIR/module_perturb_ic.sh &
-#    fi
-#    if [ $NEXTDATE -le $DATE_CYCLE_END ]; then
-#      $SCRIPT_DIR/module_wrf_ens.sh &
-#    fi
-#  fi
-#  # First deterministic run for 4DVar
-#  if $RUN_4DVAR && ! $RUN_ENKF && [ $DATE == $DATE_START ]; then
-#    $SCRIPT_DIR/module_wrf_window.sh &
-#  fi
-
-  # Data assimilation for each cycle
-  if [ $DATE -ge $DATE_CYCLE_START ] && [ $DATE -le $DATE_CYCLE_END ]; then
-    # Processing observations
-#    if $RUN_ENKF || $RUN_4DVAR; then
-#      $SCRIPT_DIR/module_obsproc.sh &
-#    fi
-#    # EnKF
-#    if $RUN_ENKF; then
-#      $SCRIPT_DIR/module_enkf.sh &
-#    fi
-#    # 4DVar
-#    if $RUN_4DVAR; then
-#      $SCRIPT_DIR/module_wrf_window1.sh &
-#      $SCRIPT_DIR/module_4dvar.sh &
-#      $SCRIPT_DIR/module_wrf_window.sh &
-#    fi
-#    # EnVar need an extra ensemble run through the obs window to get perturbations
-#    if $RUN_ENVAR; then
-#      $SCRIPT_DIR/module_wrf_ens_window1.sh &
-#    fi
-#     $SCRIPT_DIR/module_wrf_rc_truth.sh &
-     $SCRIPT_DIR/module_wrf.sh &
-  fi
-wait
-  #CHECK ERRORS
-  for d in `ls -t run/$DATE/`; do
-    if [[ `cat run/$DATE/$d/stat` == "error" ]]; then
-      echo CYCLING STOP DUE TO FAILED COMPONENT: $d
-      exit 1
-    fi
-  done
-
-  #ADVANCE TO NEXT CYCLE
-  export PREVDATE=$DATE
-  export DATE=$NEXTDATE
-done
-
-echo CYCLING COMPLETE
-
+export DATE=201510221800
+export run_minutes_cycle=`diff_time $DATE $DATE_END`
+$SCRIPT_DIR/module_wrf.sh &
