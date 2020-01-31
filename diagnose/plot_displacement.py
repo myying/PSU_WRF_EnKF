27b45ad2cb33ccd7a6c22353b0083b6e1be94015
Yue Ying <hardyying@gmail.com>
2020-01-31 10:55:18 -0700

    minor change

--- diagnose/plot_displacement.py
+++ diagnose/plot_displacement.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 import wrf_functions as wrf
 import cartopy.crs as ccrs
 
-workdir = '/glade/scratch/mying/Patricia_multiscale/run/201510230600/enkf/d02/'
+workdir = '/glade/scratch/mying/Patricia/test_201510230600/enkf_multiscale/'
 varname = 'U10'
-m = 26
-clevel = np.arange(-40, 40, 2)
+m = 1
+clevel = np.arange(-30, 32, 2)
 center_lat = 16.5
 center_lon = -105.4
 lat = wrf.getvar(workdir+'fort.80011', 'XLAT')[0, :, :]
@@ -25,11 +25,13 @@ for factor in np.arange(0.0, 1.1, 0.1):
 
   var3 = util.warp(var1, -factor*u, -factor*v)
 
-  c = ax.contourf(lon, lat, var3, clevel, cmap='seismic')
-  plt.colorbar(c)
-  ax.quiver(lon[::10, ::10], lat[::10, ::10], factor*v[::10, ::10], factor*u[::10, ::10], scale=300, headwidth=6, headlength=8, headaxislength=6, linewidths=3)
+  c = ax.contourf(lon, lat, var2, clevel, cmap='seismic')
+  cbar = plt.colorbar(c)
+  cbar.ax.tick_params(labelsize=20)
+  ax.quiver(lon[::10, ::10], lat[::10, ::10], factor*v[::10, ::10], factor*u[::10, ::10], scale=200, headwidth=6, headlength=8, headaxislength=6, linewidths=3)
+  ax.plot(center_lon, center_lat, marker='+', markersize=20, color='k')
   ax.set_aspect('equal', 'box')
-  ax.set_extent([center_lon-4, center_lon+4, center_lat-4, center_lat+4])
+  ax.set_extent([center_lon-2.5, center_lon+2.5, center_lat-2.5, center_lat+2.5])
   ax.coastlines('50m')
 
   plt.savefig("{:4.2f}.png".format(factor), dpi=200)
--- diagnose/plot_variable.py
+++ diagnose/plot_variable.py
@@ -5,25 +5,29 @@ import matplotlib.pyplot as plt
 import wrf_functions as wrf
 import cartopy.crs as ccrs
 
-workdir = '/glade/scratch/mying/Patricia_multiscale/run/201510230600/enkf/d02/'
+workdir = '/glade/scratch/mying/Patricia/test_201510230600/enkf_multiscale/'
 varname = 'U10'
-m = 26
-clevel = np.arange(-75, 75, 5)
+m = 1
+clevel = np.arange(-30, 32, 2)
 center_lat = 16.5
 center_lon = -105.4
 lat = wrf.getvar(workdir+'fort.80011', 'XLAT')[0, :, :]
 lon = wrf.getvar(workdir+'fort.80011', 'XLONG')[0, :, :]
 
-var1 = wrf.getvar(workdir+'fort.{}'.format(m+80010), varname)[0, :, :]
+var1 = wrf.getvar(workdir+'scale1/fort.{}'.format(m+50010), varname)[0, :, :]
+var1[np.where(var1>30)] = 30
+var1[np.where(var1<-30)] = -30
 
 plt.switch_backend('Agg')
 plt.figure(figsize=(8, 6))
 ax = plt.axes(projection=ccrs.PlateCarree())
 
 c = ax.contourf(lon, lat, var1, clevel, cmap='seismic')
-plt.colorbar(c)
+cbar = plt.colorbar(c)
+cbar.ax.tick_params(labelsize=20)
+ax.plot(center_lon, center_lat, marker='+', markersize=20, color='k')
 ax.set_aspect('equal', 'box')
-ax.set_extent([center_lon-4, center_lon+4, center_lat-4, center_lat+4])
+ax.set_extent([center_lon-2.5, center_lon+2.5, center_lat-2.5, center_lat+2.5])
 ax.coastlines('50m')
 
 plt.savefig("1.png", dpi=200)
