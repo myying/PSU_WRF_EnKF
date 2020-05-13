0c605c51217dcfcf5210e9bb25a44afc3216577e
Yue Ying <hardyying@gmail.com>
2020-05-12 23:18:48 -0600

    version 2 alignment algorithm: do not include amp incr

---
+++ diagnose/alignment_version2.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import numpy as np
+import util
+import wrf_functions as wrf
+import sys
+
+m = int(sys.argv[1])
+current_scale = int(sys.argv[2])
+n = 360
+nens = 60
+
+workdir = './' #'/glade/scratch/mying/Patricia_multiscale/run/201510230600/enkf/d02/'
+
+if(current_scale<3):
+  land = wrf.getvar(workdir+'fort.80011', 'HGT')
+  varname = 'U10'
+  xb = wrf.getvar(workdir+'fort.{}'.format(m+50010), varname)
+  xa = wrf.getvar(workdir+'fort.{}'.format(m+70010), varname)
+  nt, ny, nx = xb.shape
+  u1, v1 = util.optical_flow_HS(xb[0, 0:n, 0:n], xa[0, 0:n, 0:n], 5, land[0, 0:n, 0:n])
+  varname = 'V10'
+  xb = wrf.getvar(workdir+'fort.{}'.format(m+50010), varname)
+  xa = wrf.getvar(workdir+'fort.{}'.format(m+70010), varname)
+  nt, ny, nx = xb.shape
+  u2, v2 = util.optical_flow_HS(xb[0, 0:n, 0:n], xa[0, 0:n, 0:n], 5, land[0, 0:n, 0:n])
+  u = 0.5*(u1+u2)
+  v = 0.5*(v1+v2)
+
+for varname in ('U', 'V', 'W', 'P', 'PH', 'T', 'MU', 'QVAPOR', 'QCLOUD', 'QRAIN', 'QICE', 'QSNOW', 'QGRAUP', 'U10', 'V10', 'T2', 'Q2', 'PSFC'):
+  xb = wrf.getvar(workdir+'fort.{}'.format(m+50010), varname)
+  xa = wrf.getvar(workdir+'fort.{}'.format(m+70010), varname)
+  xin = wrf.getvar(workdir+'fort.{}'.format(m+90010), varname)
+
+  if(xb.ndim==4):
+    nt, nz, ny, nx = xb.shape
+  if(xb.ndim==3):
+    nt, ny, nx = xb.shape
+
+  xout = xin.copy()
+
+  if(current_scale<3):
+    if(xb.ndim==4):
+      xout[0, :, 0:n, 0:n] = util.warp(xin[0, :, 0:n, 0:n], -u, -v)
+    if(xb.ndim==3):
+      xout[0, 0:n, 0:n] = util.warp(xin[0, 0:n, 0:n], -u, -v)
+
+  wrf.writevar(workdir+'fort.{}'.format(m+90010), varname, xout)
+
